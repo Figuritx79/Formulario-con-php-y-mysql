363bde74b08da2ae13433928260c32a160fb0742
Enrique González <gonherenrique@gmail.com>
2022-10-25 14:39:57 -0500

    Página estatica(sin correción)

--- index.php
+++ index.php
@@ -18,6 +18,10 @@
         <form action="" class="registro">
             <h2 class="registro-titulo">Registra tus datos</h2>
 
+            <label for="nombre" class="registro__label">Clave:</label>
+            <input type="text" name="clave" id="clave" class="registro__input" placeholder="Clave" required autofocus>
+
+
             <label for="nombre" class="registro__label">Nombre:</label>
             <input type="text" name="nombre" id="nombre" class="registro__input" placeholder="Nombre" required autofocus>
 
@@ -25,7 +29,7 @@
             <input type="text" name="apellido" id="apellido" class="registro__input" placeholder="apellidos" required>
 
             <label for="edad" class="registro__label">Edad:</label>
-            <input type="text" name="edad" id="edad" class="registro__input" placeholder="edad" required>
+            <input type="text" name="edad" id="edad" class="registro__input" placeholder="edad" required onkeypress="return valideKey(event);">
 
             <label for="speciality" class="registro__label">Especialidad</label>
             <select name="speciality" id="speciality" class="registro__input">
@@ -37,31 +41,35 @@
 
             <label for="input-radio" class="registro__label"> Género:</label>
             <div class="input-radio">
-                <label for="M">Masculino</label>
-                <input type="radio" id="M" name="genero" id="genero" value="M">
-                <label for="F">Femenino</label>
-                <input type="radio" id="F" name="genero" id="genero" value="F">
+                <label for="M" class="radio__label">Masculino</label>
+                <input type="radio" id="M" name="genero" id="masculino" value="Masculino">
+                <label for="F" class="radio__label">Femenino</label>
+                <input type="radio" id="F" name="genero" id="femenino" value="Femenino">
             </div>
 
             <label for="input-checkbox" class="registro__label">Hobbies:</label>
             <div class="input-checkbox">
-                <label for="deporte">Deporte</label>
-                <input type="checkbox" name="hobby" id="deporte" value="deporte">
+                <div class="checkbox-1">
+                    <label for="deporte">Deporte</label>
+                    <input type="checkbox" name="hobby" id="deporte" value="deporte">
 
-                <label for="musica">Musica</label>
-                <input type="checkbox" name="hobby" id="musica" value="musica">
+                    <label for="musica">Musica</label>
+                    <input type="checkbox" name="hobby" id="musica" value="musica">
 
-                <label for="videojuegos">Videojuegos</label>
-                <input type="checkbox" name="hobby" id="videojuegos" value="videojuegos">
+                    <label for="nadar">Nadar</label>
+                    <input type="checkbox" name="hobby" id="nadar" value="nadar">
 
-                <label for="correr">Correr</label>
-                <input type="checkbox" name="hobby" id="correr" value="correr">
+                </div>
+                <div class="checkbox-2">
+                    <label for="correr">Correr</label>
+                    <input type="checkbox" name="hobby" id="correr" value="correr">
 
-                <label for="leer">Leer</label>
-                <input type="checkbox" name="hobby" id="leer" value="leer">
-                
-                <label for="dormir">Dormir</label>
-                <input type="checkbox" name="hobby" id="dormir" value="dormir">
+                    <label for="leer">Leer</label>
+                    <input type="checkbox" name="hobby" id="leer" value="leer">
+
+                    <label for="dormir">Dormir</label>
+                    <input type="checkbox" name="hobby" id="dormir" value="dormir">
+                </div>
             </div>
             <button type="submit">Enviar</button>
         </form>
